879ee67262ecac1754c357b0c6b14f9cd71a1ad0
valentine1004 <valentine1004@ukr.net>
2018-02-26 14:51:31 +0200

    redux start

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './App';
+import reducer from './reducers';
+
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
+
+
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import tracks from './tracks';
+import playlists from './playlists';
+
+export default combineReducers({
+    tracks,
+    playlists
+})
